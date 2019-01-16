0c3fe15977dcb934764d126840f254b2c267fc49
charindu <charindu87@gmail.com>
2019-01-17 01:00:02 +0800

    Delete Butterfly.java

--- Butterfly.java
+++
@@ -1,9 +0,0 @@
-package dev.singtel.interview;
-
-import dev.singtel.interview.behaviour.Flyable;
-
-public class Butterfly implements Flyable {
-    public String fly() {
-        return "I am flying";
-    }
-}
